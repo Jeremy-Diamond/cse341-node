29f42522fff8c82e89df5529c4e7d7292f2b8d46
Jeremy Diamond <socjeremyd@gmail.com>
2023-09-13 00:01:30 -0600

    lesson 1 activity

---
+++ server.js
@@ -0,0 +1,16 @@
+//express web server
+const express = require('express');
+
+//create express app
+const app = express();
+
+const lesson1Controller = require('./controllers/lesson1');
+
+app.get('/', lesson1Controller.helloRoute);
+
+app.get('/jeremy', lesson1Controller.jeremyRoute);
+
+const port = 3000;
+
+app.listen(process.env.port || port);
+console.log('Web Server is listening at port ' + (process.env.port || port));
